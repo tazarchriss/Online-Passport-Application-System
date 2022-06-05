a5e4bdab59be10d8a47f0cbd367793fb82faf178
Baltazar Christian <77143206+tazarchriss@users.noreply.github.com>
2022-06-05 22:54:15 +0300

    Create criminal.php

---
+++ configurations/criminal.php
@@ -0,0 +1,20 @@
+<?php
+session_start();
+
+require_once('connection.php');
+$id=$_GET['id'];
+echo $id;
+
+
+$sql="UPDATE `passport_request` SET `req_status`='criminal' WHERE `req_id`='$id'";
+$qry=mysqli_query($conn,$sql);
+
+if (!$qry) {
+    die(mysqli_error($conn));
+}
+else
+{
+ header('Location:../s_reports.php?updated');
+}
+
+?>
